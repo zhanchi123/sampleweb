4d8e7e5a2394e310d8cd29605b92cc104f137215
13818408248@163.com <13818408248Ch>
2021-04-23 20:27:53 +0800

    重命名文件名称，由Dev变为DevTest

---
+++ src/main/java/sampleweb/DevTest.java
@@ -0,0 +1,28 @@
+package sampleweb;
+
+public class DevTest {
+	public void printInfo() {
+		System.out.println("wo shi dev.");
+	}
+	
+	public void getCommit() {
+		System.out.println("Di er ci ti jiao.");
+	}
+	
+	public void test() {
+		System.out.println("wo yao ce shi le.");
+	}
+	
+	public void getCommit2() {
+		System.out.println("hai shi bu dui.");
+	}
+	
+	public void getCommit3() {
+		System.out.println("第五次提交");
+	}
+
+
+	public void renameTest(){
+		System.out.println("测试文件重命名，又Dev变为DevTest");
+	}
+}
